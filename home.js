df27776c3bcbbf31f0e7e6e6dff334faecf00fde
kulsoomnawab-7 <105455181+kulsoomnawab-7@users.noreply.github.com>
2023-01-13 14:33:12 +0500

    Add files via upload

---
+++ home.js
@@ -0,0 +1,17 @@
+
+//
+const headerBg = () => {
+    const header = document.querySelector(".js-header");
+    window.addEventListener("scroll",function()
+    {
+        if(this.scrollY > 0)
+        {
+            header.classList.add("bg-reveal");
+        }
+        else
+        {
+            header.classList.remove("bg-reveal");
+        }
+    });
+}
+headerBg();
